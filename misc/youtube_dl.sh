90b702ff948deeccef1fac76e945e571a41aee6e
Saverio <xavier.mitault@epitech.eu>
2022-01-05 15:17:06 +0100

    [add] misc/youtube_dl.sh: recode audio to ogg if an option is provided

--- misc/youtube_dl.sh
+++ misc/youtube_dl.sh
@@ -17,10 +17,20 @@ fi
 
 if [ $ECHO_HELP -eq 1 ]
 then
-	echo "bash youtube_dl.sh URL [directory_path]"
-	echo "download online audio with URL"
+	echo "USAGE:"
+	echo "bash youtube_dl.sh URL [directory_path [ogg]]"
+	echo
+	echo "download online audio with URL, example:"
+	echo "bash youtube_dl.sh https://www.youtube.com/watch?v=f_zpcGKgXgw"
+	echo
 	echo "if you provide a directory path, it will download file here,"
-	echo "else it will donwload to the current directory is used"
+	echo "else it will donwload to the current working directory, example:"
+	echo "bash youtube_dl.sh https://www.youtube.com/watch?v=f_zpcGKgXgw something/"
+	echo
+	echo "and if you want to download the video using ogg format,"
+	echo "please provide a path and write ogg after, example:"
+	echo "bash youtube_dl.sh https://www.youtube.com/watch?v=f_zpcGKgXgw . ogg"
+	echo
 	exit 0
 fi
 
@@ -31,6 +41,13 @@ else
 	DIR_PATH=$2
 fi
 
+if [ "$3" == "ogg" ]
+then
+	OGG_COMMAND='--recode-video ogg'
+else
+	OGG_COMMAND=''
+fi
+
 if ! command -v youtube-dl &> /dev/null
 then
         echo "you need to install 'youtube-dl' package"
@@ -39,4 +56,4 @@ fi
 echo "download audio"
 youtube-dl --ignore-config --geo-bypass --yes-playlist --restrict-filenames \
 	--no-cache-dir --format 'bestaudio[ext=mp3]/bestaudio' $1 \
-	-o "$DIR_PATH/%(title)s.%(ext)s"
+	-o "$DIR_PATH/%(title)s.%(ext)s" $OGG_COMMAND
