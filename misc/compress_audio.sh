d4854dd1a568ead2d45da6999957b64e825e58de
saverio <xavier.mitault@epitech.eu>
2022-02-28 19:06:01 +0100

    Add script (but mainly to reminds the commands)

---
+++ misc/compress_audio.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if ! command -v ffmpeg &> /dev/null
+then
+    echo "install ffmpeg first"
+    exit 84
+fi
+for FILE in $(/usr/bin/ls $1)
+do
+    ffmpeg -y -i $1 -c:a libvorbis -qscale 1 $1
+done
