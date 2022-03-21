d5544a1ff203c7bced2c7e509adcc5e9d8e99a43
saverio <xavier.mitault@epitech.eu>
2022-03-21 01:42:38 +0100

    Better render

--- text-editor/vim/.ycm_global_ycm_extra_conf.py
+++ text-editor/vim/.ycm_global_ycm_extra_conf.py
@@ -1,7 +1,22 @@
 def Settings( **kwargs ):
-    return {
-        'flags': [ '-x', 'c', '-Wall', '-Wextra', '-Werror',
-            '-Ilib/include', '-Iinclude', '-I../include', '-I../lib/include',
-            '-Ilib/includes', '-Iincludes', '-I../includes',
-            '-I../lib/includes'],
+    sttings = {
+        'flags': [
+            '-x',
+            'c',
+            '-Ilib/include',
+            '-Iinclude',
+            '-I../include',
+            '-I../lib/include',
+            '-Ilib/includes',
+            '-Iincludes',
+            '-I../includes',
+            '-I../lib/includes',
+            '-Ilib/libbgs/include',
+            '-Igenerator/include',
+            '-Isolver/include',
+            '-Wall',
+            '-Wextra',
+            '-Wpedantic'
+        ],
     }
+    return sttings
