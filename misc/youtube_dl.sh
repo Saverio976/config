1c0c9e8870078df26cb1d737e3a7e159c5be67e0
saverio <xavier.mitault@epitech.eu>
2022-04-27 15:31:15 +0200

    Add flag to ignore errors

--- misc/youtube_dl.sh
+++ misc/youtube_dl.sh
@@ -56,4 +56,4 @@ fi
 echo "download audio"
 youtube-dl --ignore-config --geo-bypass --yes-playlist --restrict-filenames \
 	--no-cache-dir --format 'bestaudio[ext=mp3]/bestaudio' $1 \
-	-o "$DIR_PATH/%(title)s.%(ext)s" $OGG_COMMAND
+	-o "$DIR_PATH/%(title)s.%(ext)s" $OGG_COMMAND --ignore-errors
