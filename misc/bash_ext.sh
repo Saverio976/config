ec694156d230efc3f9622bafed8159d2b60c5a0c
Xavier Mitault <xavier.mitault@epitech.eu>
2022-05-24 21:15:48 +0200

    Re organanize alias + replacements

--- misc/bash_ext.sh
+++ misc/bash_ext.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
-# conf
-alias i3conf="nvim $HOME/.config/i3/config"
-alias zshconf="nvim $HOME/.zshrc"
-alias vimconf="nvim $HOME/.vimrc"
-alias starshipconf="nvim $HOME/.config/starship.toml"
-
 # im in the vim matrix
 alias :q="exit"
 
 alias searchd="s-d"
 alias searchf="s-f"
-alias scrcpy="scrcpy -S -w --power-off-on-close --disable-screensaver"
 alias restartx="sudo systemctl restart lightdm.service"
-alias normatrix="$HOME/source/NorMatrix/main.py"
-
-alias audio-input-toggle="pactl set-source-mute @DEFAULT_SOURCE@ toggle"
-alias audio-output-toggle="pactl set-sink-mute @DEFAULT_SINK@ toggle"
 
 # prompt gpg password on tty
 GPG_TTY=$(tty)
 export GPG_TTY
 
+##############################################################################
+# REPLACE COMMAND
 function replace_command() {
     command_replace=$(echo $2 | awk '{print $1;}')
     if command -v $command_replace &> /dev/null
@@ -34,15 +25,49 @@ replace_command "ls" "exa -la"
 replace_command "cat" "bat"
 replace_command "htop" "btm"
 
+# replace cd command
+if command -v zoxide &> /dev/null
+then
+    eval "$(zoxide init zsh --cmd cd)"
+fi
+
+##############################################################################
+# ALIAS
+function alias_if_exists() {
+    command_exists=$(echo $2 | awk '{print $1;}')
+    if command -v $command_exists &> /dev/null
+    then
+        alias $1="$2"
+    fi
+}
+
+alias_if_exists "audio-input-toggle" "pactl set-source-mute @DEFAULT_SOURCE@ toggle"
+# alias audio-input-toggle="pactl set-source-mute @DEFAULT_SOURCE@ toggle"
+alias_if_exists "audio-output-toggle" "pactl set-sink-mute @DEFAULT_SINK@ toggle"
+# alias audio-output-toggle="pactl set-sink-mute @DEFAULT_SINK@ toggle"
+alias_if_exists "scrcpy" "scrcpy -S -w --power-off-on-close --disable-screensaver"
+# alias scrcpy="scrcpy -S -w --power-off-on-close --disable-screensaver"
+alias_if_exists "normatrix" "$HOME/source/NorMatrix/main.py"
+# alias normatrix="$HOME/source/NorMatrix/main.py"
+
+alias_if_exists "termtosvg" "python3 -m termtosvg"
+
+alias_if_exists "i3conf" "nvim $HOME/.config/i3/config"
+# alias i3conf="nvim $HOME/.config/i3/config"
+alias_if_exists "zshconf" "nvim $HOME/.zshrc"
+# alias zshconf="nvim $HOME/.zshrc"
+alias_if_exists "starshipconf" "nvim $HOME/.config/starship.toml"
+# alias starshipconf="nvim $HOME/.config/starship.toml"
+
+##############################################################################
+# PROMPT
 if command -v starship &> /dev/null
 then
     eval "$(starship init zsh)"
 fi
 
-if command -v zoxide &> /dev/null
-then
-    eval "$(zoxide init zsh --cmd cd)"
-fi
+##############################################################################
+# CUSTOM FUNCTION
 
 # search directory name
 function s-d () {
@@ -81,6 +106,21 @@ function cleanpacman () {
     fi
 }
 
+# clean paru cache
+function cleanparu () {
+    OLDPKG=$(paru -Qdtq)
+    if [[ "$OLDPKG" != "" ]]
+    then
+        sudo paru -Rns $OLDPKG
+    fi
+    if [[ "$1" == "y" ]]
+    then
+        yes | sudo paru -Scc
+    else
+        sudo paru -Scc
+    fi
+}
+
 # clean yay cache
 function cleanyay () {
     OLDPKG=$(yay -Qdtq)
@@ -96,10 +136,11 @@ function cleanyay () {
     fi
 }
 
+true_man_intern=$(which man)
 function man () {
     if [[ "$1" == "google" ]]; then
         xdg-open 'https://duckduckgo.com/?q='$2
     else
-        /bin/man "$@"
+        $true_man_intern "$@"
     fi
 }
