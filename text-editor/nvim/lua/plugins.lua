abfd905b317bf4357a7ab61e966f3401a4f0f367
Xavier Mitault <xavier.mitault@epitech.eu>
2022-06-08 21:18:54 +0000

    Add pluggins

--- text-editor/nvim/lua/plugins.lua
+++ text-editor/nvim/lua/plugins.lua
@@ -368,6 +368,9 @@ return require('packer').startup(function(use)
     use {
         'rcarriga/nvim-notify',
         config = function ()
+            require("notify").setup({
+                background_colour = "#000000"
+            })
             vim.notify = require("notify")
         end
     }
@@ -408,12 +411,47 @@ return require('packer').startup(function(use)
 
     use {
         's1n7ax/nvim-comment-frame',
-        requires = {
-            { 'nvim-treesitter' }
-        },
+        requires = { { 'nvim-treesitter' } },
+        config = function() require('nvim-comment-frame').setup() end
+    }
+
+    use {
+        'axieax/urlview.nvim',
         config = function()
-            require('nvim-comment-frame').setup()
+            require("urlview").setup({
+                -- Prompt title (`<context> <default_title>`, e.g. `Buffer Links:`)
+                default_title = "Links:",
+                -- Default picker to display links with
+                -- Options: "native" (vim.ui.select) or "telescope"
+                default_picker = "telescope",
+                -- Set the default protocol for us to prefix URLs with if they don't start with http/https
+                default_prefix = "https://",
+                -- Command or method to open links with
+                -- Options: "netrw", "system" (default OS browser); or "firefox", "chromium" etc.
+                navigate_method = "system",
+                -- Ensure links shown in the picker are unique (no duplicates)
+                unique = true,
+                -- Ensure links shown in the picker are sorted alphabetically
+                sorted = true,
+                -- Logs user warnings (recommended for error detection)
+                debug = true,
+                -- Custom search captures
+                -- NOTE: captures follow Lua pattern matching (https://riptutorial.com/lua/example/20315/lua-pattern-matching)
+                custom_searches = {
+                    -- KEY: search source name
+                    -- VALUE: custom search function or table (map with keys capture, format)
+                    jira = {
+                        capture = "AXIE%-%d+",
+                        format = "https://jira.axieax.com/browse/%s",
+                    },
+                },
+            })
+            require("telescope").load_extension("urlview")
         end
     }
+
+    use { 'hrsh7th/vim-searchx' }
+
+    use { 'adelarsq/vim-emoji-icon-theme' }
 
 end)
