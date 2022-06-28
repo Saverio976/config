c22d51df2124548567e06354cc70abe3b0b44857
Xavier Mitault <xavier.mitault@epitech.eu>
2022-06-28 12:23:45 +0200

    specify path to ohmyzsh

--- install.sh
+++ install.sh
@@ -38,6 +38,7 @@ done
 sudo systemctl enable --now NetworkManager
 sudo systemctl enable --now dhcpcd
 
+export ZSH="$HOME/.config/oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
